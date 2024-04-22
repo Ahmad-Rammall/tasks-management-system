6764f1ad6d684ad6aea8ce467d61dd6a01ec81bb
Ahmad-Rammall <ahmad.rammal2023@gmail.com>
2024-04-22 16:16:57 +0300

    API <> Create UserRole + User Models

---
+++ TasksManagementSystem.API/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TasksManagementSystem.API.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public bool isDeleted { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
--- TasksManagementSystem.API/obj/Debug/net8.0/TasksManagementSystem.API.AssemblyInfo.cs
+++ TasksManagementSystem.API/obj/Debug/net8.0/TasksManagementSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasksManagementSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625ed45b9135f13de6f802c1820a8c06a730d65b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasksManagementSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasksManagementSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
