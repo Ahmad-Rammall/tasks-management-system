e5cba84a2f0f902f41508ce7c099936050659f88
Ahmad-Rammall <ahmad.rammal2023@gmail.com>
2024-04-22 16:24:09 +0300

    API <> Create Project + Task Models

---
+++ TasksManagementSystem.API/Entities/Task.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TasksManagementSystem.API.Entities
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("UserProp")]
+        public int UserId {  get; set; }
+
+        [ForeignKey("ProjectProp")]
+        public int ProjectId { get; set; }
+        public bool IsCompleted {  get; set; }
+        public User UserProp {  get; set; }
+        public Project ProjectProp { get; set; }
+    }
+}
--- TasksManagementSystem.API/obj/Debug/net8.0/TasksManagementSystem.API.AssemblyInfo.cs
+++ TasksManagementSystem.API/obj/Debug/net8.0/TasksManagementSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasksManagementSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625ed45b9135f13de6f802c1820a8c06a730d65b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6764f1ad6d684ad6aea8ce467d61dd6a01ec81bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasksManagementSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasksManagementSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
