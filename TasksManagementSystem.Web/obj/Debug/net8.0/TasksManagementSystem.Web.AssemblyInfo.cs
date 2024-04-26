94c3465552c623970528d53c7de2f0ea0778fe27
Ahmad-Rammall <ahmad.rammal2023@gmail.com>
2024-04-26 21:21:53 +0300

    Frontend <> Create Comment Component

--- TasksManagementSystem.Web/obj/Debug/net8.0/TasksManagementSystem.Web.AssemblyInfo.cs
+++ TasksManagementSystem.Web/obj/Debug/net8.0/TasksManagementSystem.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasksManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7adff0a0f4071b72a81532abee694e08e0981def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b7fe4fda43a8382b0951b45ae397cef95e728f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasksManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasksManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
