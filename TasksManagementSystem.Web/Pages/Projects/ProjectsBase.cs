e81b55c7549fb6f2e78f62cea9c802e61504371e
Ahmad-Rammall <ahmad.rammal2023@gmail.com>
2024-04-27 15:55:49 +0300

    Frontend <> Create Admin Pages

---
+++ TasksManagementSystem.Web/Pages/Projects/ProjectsBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TasksManagementSystem.Web.Pages.Projects
+{
+    public class ProjectsBase : ComponentBase
+    {
+    }
+}
---
+++ TasksManagementSystem.Web/Pages/Requests/RequestsBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TasksManagementSystem.Web.Pages.Requests
+{
+    public class RequestsBase : ComponentBase
+    {
+    }
+}
